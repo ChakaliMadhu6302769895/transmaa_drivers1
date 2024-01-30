e9624d746c9d1b5a1eedbf95dbddfb0582611ae5
ChakaliMadhu6302769895 <gauthamroyalz12324@gmail.com>
2024-01-30 13:35:49 +0530

    Almost done...

--- lib/screens/Available_loads.dart
+++ lib/screens/Available_loads.dart
@@ -18,7 +18,9 @@ class _MyHomePageState extends State<MyHomePage> {
         color: Color(0xfff5f5f5),
         child: Column(
           children: [
-            SizedBox(height: 40,),
+            SizedBox(
+              height: 40,
+            ),
             Container(
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -31,39 +33,52 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               ),
-              padding: EdgeInsets.only(top: 10 , right: 10),
               height: 70,
               width: double.infinity,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    child: Image.asset(
-                      'assets/Finalized_logo_TRANSMAA-removebg-preview.png',
-                      fit: BoxFit.cover,
-                      height: 65,
-                      width: 230,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20 , bottom: 5),
+                    child: Container(
+                      child: Image.asset(
+                        'assets/Final logo.png',
+                        fit: BoxFit.cover,
+                        height: 45,
+                        width: 161,
+                      ),
                     ),
                   ),
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.notifications_none_rounded ,size: 30,color: Colors.black,),
-                        onPressed: () {
-                          // Handle notification icon press
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.help_outline_outlined , size: 30, color: Colors.black,),
-                        onPressed: () {
-                          // Handle help icon press
-                        },
-                      ),
-                    ],
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.notifications_none_rounded,
+                            size: 30,
+                            color: Colors.black,
+                          ),
+                          onPressed: () {
+                            // Handle notification icon press
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.help_outline_outlined,
+                            size: 30,
+                            color: Colors.black,
+                          ),
+                          onPressed: () {
+                            // Handle help icon press
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
+
             SizedBox(height: 10),
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -87,107 +102,150 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               height: 260,
               width: 370,
-              padding: EdgeInsets.all(16.0),
-              child: Column(
+              padding: EdgeInsets.all(16),
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 4,
-                          ),
-                        ),
-                      ),
-                      Icon(
-                        Icons.circle_outlined,
-                        color: Colors.transparent,
-                       size: 1,
-                      ),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
                           children: [
-                            Text(
-                              'From',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
+                            Container(
+                              width: 18,
+                              height: 18,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 4,
+                                ),
                               ),
                             ),
-                            SizedBox(height: 5,),
-                            TextField(
-                              controller: fromLocationController,
-                              decoration: InputDecoration(
-                                hintText: 'Load it....',
-                                border: OutlineInputBorder(),
-                                contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                            Icon(
+                              Icons.circle_outlined,
+                              color: Colors.transparent,
+                              size: 1,
+                            ),
+                            SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Text(
+                                    'From',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  TextField(
+                                    controller: fromLocationController,
+                                    decoration: InputDecoration(
+                                      hintText: 'Load it....',
+                                      border: OutlineInputBorder(),
+                                      contentPadding: EdgeInsets.symmetric(
+                                        horizontal: 15,
+                                        vertical: 10,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ],
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Icon(Icons.location_on_outlined, color: Colors.red, size: 22),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                        SizedBox(height: 10),
+                        Row(
                           children: [
-                            Text(
-                              'To',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20),
+                              child: Icon(
+                                Icons.location_on_outlined,
+                                color: Colors.red,
+                                size: 20,
                               ),
                             ),
-                            SizedBox(height: 5,),
-                            TextField(
-                              controller: toLocationController,
-                              decoration: InputDecoration(
-                                hintText: 'Unload to....',
-                                border: OutlineInputBorder(),
-                                contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                            SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text(
+                                        'To',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  TextField(
+                                    controller: toLocationController,
+                                    decoration: InputDecoration(
+                                      hintText: 'Unload to....',
+                                      border: OutlineInputBorder(),
+                                      contentPadding: EdgeInsets.symmetric(
+                                        horizontal: 15,
+                                        vertical: 10,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ],
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 28)),
-                      ElevatedButton(
-                        onPressed: () {
-
-                        },
-                        style: ElevatedButton.styleFrom(
-                          fixedSize: Size(310, 40),
-                          primary: Colors.grey[400],
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Padding(padding: EdgeInsets.only(left: 30)),
+                            ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: Size(275, 40),
+                                primary: Colors.grey[400],
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child: Text(
+                                'Search',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        child: Text(
-                          'Search',
-                          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18, color: Colors.black),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-
+                  SizedBox(width: 10),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 50),
+                          child: Image.asset("assets/Vector.png"),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
